2ed47353bb4693c496698e9d348f23ec62c0428e
flavionam <flavionam@gmail.com>
2013-09-17 23:18:30 -0300

    delete file

---
+++ ProblemApplication/ProblemApplication/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ProblemApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnResolver = new System.Windows.Forms.Button();
+            this.dgUsuarios = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnResolver
+            // 
+            this.btnResolver.Location = new System.Drawing.Point(33, 30);
+            this.btnResolver.Name = "btnResolver";
+            this.btnResolver.Size = new System.Drawing.Size(132, 23);
+            this.btnResolver.TabIndex = 0;
+            this.btnResolver.Text = "Resolver o problema";
+            this.btnResolver.UseVisualStyleBackColor = true;
+            this.btnResolver.Click += new System.EventHandler(this.btnResolver_Click);
+            // 
+            // dgUsuarios
+            // 
+            this.dgUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgUsuarios.Location = new System.Drawing.Point(12, 105);
+            this.dgUsuarios.Name = "dgUsuarios";
+            this.dgUsuarios.Size = new System.Drawing.Size(635, 217);
+            this.dgUsuarios.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 334);
+            this.Controls.Add(this.dgUsuarios);
+            this.Controls.Add(this.btnResolver);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgUsuarios)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnResolver;
+        private System.Windows.Forms.DataGridView dgUsuarios;
+    }
+}
+
---
+++ ProblemApplication/ProblemApplication/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BO;
+
+namespace ProblemApplication
+{
+     
+
+    public partial class Form1 : Form
+    {
+        BO_Usuario bo_usuario = new BO_Usuario();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnResolver_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //a camada de apresentação não enxerga a DAL.
+                //Para uma aplicação mais elaborada, provavelmente teria utilizado o MVC
+                bo_usuario.preencherDataGrid(ref dgUsuarios);     
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Erro na aplicação, favor entrar em contato com suporte.", "ERRO");
+            }
+                
+            
+        }
+    
+    }
+}
